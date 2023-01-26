34c37188d9fcbf7aae6a112f25b17f9468733485
Longs Pemun Gotar <developerlongs@gmail.com>
2023-01-26 15:21:07 +0000

    waitgroup

--- main.go
+++ main.go
@@ -2,14 +2,33 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"sync"
+	"time"
 )
 
-func main(){
-	fmt.Println("Main execution started")
-	fmt.Println("NO. of CPUs available:", runtime.NumCPU())
-	fmt.Println("NO. of Goroutines available:", runtime.NumGoroutine())
+func f1(wg *sync.WaitGroup){
+	fmt.Println("f1 goroutine execution started")
+	for i := 0; i < 3; i++{
+		fmt.Println("f1, i=", i)
+		time.Sleep(time.Second)
+	}
+	wg.Done()
+}
 
-	fmt.Println("OS:", runtime.GOOS)
-	fmt.Println("Arch:", runtime.NumCPU())
+func f2(){
+	fmt.Println("f2 goroutine execution started")
+	for i := 5; i < 8; i++{
+		fmt.Println("f2, i=", i)
+	}
+}
+func main(){
+	var wg sync.WaitGroup
+	wg.Add(1)
+	
+	go f1(&wg)
+	
+
+	f2()
+
+	fmt.Println("Main execution stopped")
 }
